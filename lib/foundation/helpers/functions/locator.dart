a3892d6c144f407fd6f4d304c7c61670bf296405
Elnur Ibrahimov <elnuribrahmv@gmail.com>
2023-12-02 22:07:58 +0400

    deleting unsed bloc

--- lib/foundation/helpers/functions/locator.dart
+++ lib/foundation/helpers/functions/locator.dart
@@ -1,6 +1,5 @@
 import 'package:azercell_task_application/foundation/state_logic/cubits/home/home_cubit.dart';
 import 'package:azercell_task_application/foundation/state_logic/vanilla_blocs/app_bloc.dart';
-import 'package:azercell_task_application/foundation/state_logic/vanilla_blocs/root_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,7 +9,6 @@ final ctx = navigatorKey.currentContext;
 
 Future<void> setupLocator() async {
   // STATE
-  getIt.registerLazySingleton(() => RootBloc(), dispose: (i) => i.close());
   getIt.registerLazySingleton(() => AppBloc(), dispose: (i) => i.close());
   getIt.registerLazySingleton(() => HomeCubit(), dispose: (i) => i.close());
 }
--- lib/foundation/state_logic/vanilla_blocs/app_bloc.dart
+++ lib/foundation/state_logic/vanilla_blocs/app_bloc.dart
@@ -1,15 +1,12 @@
 import 'package:azercell_task_application/foundation/helpers/functions/locator.dart';
 import 'package:azercell_task_application/foundation/models/data_containers/in_memory_store.dart';
 import 'package:azercell_task_application/foundation/models/data_containers/user_info.dart';
-import 'package:azercell_task_application/foundation/state_logic/vanilla_blocs/root_bloc.dart';
 import 'package:azercell_task_application/presentation/screens/home/home_screen.dart';
 import 'package:azercell_task_application/presentation/screens/auth/register_screen.dart';
 import 'package:azercell_task_application/utilities/extensions/widget_extentions.dart';
 import 'package:flutter/material.dart';
 
 class AppBloc {
-  RootBloc get _rootBloc => getIt<RootBloc>();
-
   final _userInfo = InMemoryStore<UserInfo?>(null);
 
   Stream<UserInfo?> get userInfo$ => _userInfo.stream;
@@ -26,7 +23,6 @@ class AppBloc {
     Navigator.of(ctx!)
         .pushAndRemoveUntil(const LoginScreen().route, (_) => false);
 
-    _rootBloc.jumpToBnbTab(0);
     updateUserInfo(null);
   }
 
